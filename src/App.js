57ed29c3970f9405664ee07455256ce7a3d42edb
Miguel-Liranzo <94998643+Miguel-Liranzo@users.noreply.github.com>
2023-03-01 21:47:26 -0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out your boy!
         </a>
       </header>
     </div>
